833d7d584ff4f5ca40c060351bf27efc7175d264
David Reis <davidsilvareis23@gmail.com>
2026-03-17 11:18:45 -0300

    feat: atualiza dependências do React e adiciona página de registro

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import Footer from '@/components/shared/footer'
 import Navbar from '@/components/shared/navbar'
 import HomePage from '@/pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import { Outlet } from 'react-router'
 
 function App() {
   return (
     <>
-      <div className='flex flex-col min-h-svh'>
+      <div className="flex flex-col min-h-svh">
         <Navbar />
-        <main className='flex-1 flex flex-col'>
-          {/* <HomePage /> */}
-          <LoginPage />
+        <main className="flex-1 flex flex-col">
+          <Outlet />
         </main>
         <Footer />
       </div>
